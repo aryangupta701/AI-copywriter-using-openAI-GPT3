318a45110a3dbbc90e3defbde45464903bb14a8e
aryangupta701 <garyan447@gmail.com>
2022-12-05 15:42:38 +0530

    api working correctly

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: "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",
+  });
+const openai = new OpenAIApi(configuration);
+
 
 const Body = () => {
 
-    const apiCall  = ()=>{
-        return "heelo"
-    }
-
     const [productName, setProductName] = useState("")
     const [description, setDescription] = useState("")
-    const [tone, setTone] = useState("sdf")
-    const submitButtonHandler = (event)=>{
-        event.preventDefault()
-        const values = apiCall()
+    const [output, setOutput] = useState("")
+    const [tone, setTone] = useState("")
+
+    const callApi = async() =>{
+       
+       try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `create a array of three decriptions of the product below\n\nproduct name : ${productName} \ndescription : ${description}\ntone : ${tone}\n`,
+            temperature: 0.7,
+            max_tokens: 1097,
+            top_p: 1,
+            frequency_penalty: 0.09,
+            presence_penalty: 0.09,
+            })
+            console.log(response?.data)
+            setOutput(response?.data.choices[0].text)
+        } catch (error) {
+        console.log(error.message)
+       }
     }
 
+    const submitButtonHandler = async(event)=>{
+        event.preventDefault()
+        console.log("before");
+        callApi();
+       console.log("afterHit");
+    }
+
+   
     const toneHandler = (e)=>{
         setTone (e.target.value)
     }
@@ -34,6 +60,8 @@ const Body = () => {
             <input value ={description} onChange={desciptionHandler}></input>
             <label >Tone</label>
             <input value ={tone} onChange={toneHandler}></input>
+            <label >Output</label>
+            <input value ={output}></input>
             <button onClick={submitButtonHandler}>Submit</button>
         </form>
     </div>
